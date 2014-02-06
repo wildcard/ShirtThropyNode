edb13385229c735c05aabcfe214ea2db1c1f13c2
Livshitz <eli.livshitz@gmail.com>
2014-02-07 00:23:30 +0200

    adding amount to donate

--- routes/donate.js
+++ routes/donate.js
@@ -8,6 +8,7 @@ exports.donate = function(req, res){
     // TODO: call contoller to
     var profile = {
         id: req.params.id,
+        image: '/public/img/Homelessman.jpg',
         fullname: 'Charles Homelessman',
         description: 'Was a British politician who was the Prime Minister of the United Kingdom from 1940 to 1945 and again from 1951 to 1955. Widely regarded as one of the greatest wartime leaders of the 20th century, Churchill was also an officer in the British Army, a historian, a writer, and an artist. He is the only British Prime Minister to have won the Nobel Prize in Literature, and was the first person to be made an honorary citizen of the United States.',
         donation_current: 69.34,
